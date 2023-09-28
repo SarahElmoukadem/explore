96ac70c9d8770cea55d369242b2b13f5d5252f10
SarahElmoukadem <sarahelmoukadem.se@gmail.com>
2023-09-28 13:08:04 +0300

    add the app screens

---
+++ App/screens/Home.js
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import Header from '../Components/Header'
+
+export default function Home() {
+  return (
+    <View style={{paddingTop:50,paddingLeft:20,paddingRight:20}}>
+        <Header />
+    </View>
+  )
+}
---
+++ App/screens/Profile.js
@@ -0,0 +1,14 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function Profile() {
+  return (
+    <View>
+      <Text>Profile</Text>
+      <Text>Profile</Text>
+      <Text>Profile</Text>
+      <Text>Profile</Text>
+      <Text>Profile</Text>
+    </View>
+  )
+}
